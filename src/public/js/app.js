8cde39219ae402ac28dfd074c9df4d4134d45ef6
Alang Kim <rlaangh77@naver.com>
2022-02-15 00:06:56 +0900

    feat(frontend): webSocket connection

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,20 @@
-console.log("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+console.log(socket);
+
+socket.addEventListener("open", () => {
+  console.log("서버에 연결되었습니다😃");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log(`Message: ${message.data}`);
+  console.log(message);
+});
+
+socket.addEventListener("close", () => {
+  console.log("서버와의 연결이 끊겼습니다");
+});
+
+setTimeout(() => {
+  socket.send("Hello from the browser");
+}, 10000);
