97c1848f8a56c6d2ded83857bcb1e4b65ca330c4
Alang Kim <rlaangh77@naver.com>
2022-02-15 00:06:29 +0900

    feat(backend): webSocket connection

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -7,5 +9,23 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wsServer = new WebSocketServer({ server });
+
+wsServer.on("connection", (socket) => {
+  const handleClose = () => {
+    console.log("브라우저 연결이 끊겼습니다😅");
+  };
+  const handleMessage = (message) => {
+    console.log(message.toString("utf8"));
+  };
+  console.log("브라우저가 연결되었습니다😄");
+  socket.send("hello!");
+  socket.on("message", handleMessage);
+  socket.on("close", handleClose);
+});
+
+server.listen(3000, handleListen);
