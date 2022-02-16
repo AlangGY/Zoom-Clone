0bbadad057598f9be4f35d161a03dc16ff61cff1
Alang Kim <rlaangh77@naver.com>
2022-02-17 01:15:56 +0900

    feat(style): style 추가

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+app.use("/css", express.static(__dirname + "/views/css"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
