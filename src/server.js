343c0f03ad2d0fdbb4e1afbc859ea2c35ad5d6f5
Alang Kim <rlaangh77@naver.com>
2022-02-17 00:11:22 +0900

    feat(backend): send roomList

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from "http";
 import SocketIO from "socket.io";
 import express from "express";
-import { getEnteredRoom } from "./libs/util/functions";
+import { getEnteredRoom, getPublicRooms } from "./libs/util/functions";
 
 const app = express();
 
@@ -17,44 +17,57 @@ const server = http.createServer(app);
 const io = SocketIO(server);
 
 io.on("connection", (socket) => {
+  const sendRoomList = () => {
+    const { rooms, sids } = socket.adapter;
+    const publicRooms = getPublicRooms([...rooms], sids);
+    // roomsIter.filter(())
+    io.emit("room_list", { roomList: publicRooms });
+  };
+
   socket.onAny((event) => {
     console.log(`Socket Event: ${event}`);
-    switch (event) {
-      case ("enter_room", "leave_room"):
-        console.log(socket.adapter.rooms);
-        break;
-    }
   });
+  sendRoomList();
   socket.on("enter_room", ({ room }, done) => {
     socket.join(room);
-    console.log(socket.adapter.rooms);
     socket
       .to(room)
       .emit("announce", { type: "join", nickname: socket["__nickname"] });
+    sendRoomList();
+
     done?.();
   });
+
   socket.on("leave_room", ({ room }, done) => {
     socket
       .to(room)
       .emit("announce", { type: "leave", nickname: socket["__nickname"] });
     socket.leave(room);
+    sendRoomList();
+
     done?.();
   });
+
   socket.on("disconnecting", () => {});
+
   socket.on("disconnect", () => {
+    const rooms = getEnteredRoom([...socket.rooms], socket.id);
     socket
-      .to(room)
+      .to(rooms)
       .emit("announce", { type: "leave", nickname: socket["__nickname"] });
+    sendRoomList();
   });
+
   socket.on("change_nickname", ({ nickname }, done) => {
     const prevNickname = socket["__nickname"];
     socket["__nickname"] = nickname;
-    const rooms = getEnteredRoom([...socket.adapter.rooms], socket.id);
+    const rooms = getEnteredRoom([...socket.rooms], socket.id);
     socket
       .to(rooms)
       .emit("announce", { type: "change-nickname", nickname, prevNickname });
     done?.();
   });
+
   socket.on("new_chat", ({ chat, room }, done) => {
     console.log(chat, room, socket["__nickname"]);
     socket.to(room).emit("new_chat", { nickname: socket["__nickname"], chat });
