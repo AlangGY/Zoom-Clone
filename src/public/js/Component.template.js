485b5398889f921e3d68cf4230cbd468400e5a5a
Alang Kim <rlaangh77@naver.com>
2022-02-18 00:56:03 +0900

    feat(frontend): component children 기능 추가

--- src/public/js/Component.template.js
+++ src/public/js/Component.template.js
@@ -1,11 +1,19 @@
 class Component {
   node;
+  childFragment;
+  children;
 
-  constructor($target, initialState = {}) {
+  constructor($target, initialState = {}, ...children) {
     this._id = ++document.nextId;
     document.componentRegistry[this._id] = this;
     this.$target = $target;
     this.state = { ...initialState };
+    if (children) {
+      this.childFragment = document.createDocumentFragment();
+      this.children = children.map(([ChildComponent, ...props]) =>
+        new ChildComponent(this.childFragment, ...props).mount()
+      );
+    }
   }
 
   template() {
@@ -13,27 +21,42 @@ class Component {
   }
 
   render() {
-    console.log("render!");
-    this.clearEvent();
-    if (this.node) this.node.innerHTML = this.template();
-    if (this.node instanceof DocumentFragment)
+    if (this.node instanceof DocumentFragment) {
       this.node.appendChild(this.template());
-    this.setEvent();
+    } else if (this.node) {
+      this.clearEvent();
+      this.node.innerHTML = this.template();
+      this.setEvent();
+    }
+    if (this.childFragment && this.children) {
+      this.children.forEach((ChildComponentInstance) =>
+        ChildComponentInstance.render()
+      );
+    }
     return this;
   }
 
   setState(newState) {
     this.state = { ...this.state, ...newState };
+    if (this.childFragment && this.children) {
+      this.children.forEach((ChildComponentInstance) =>
+        ChildComponentInstance.setState(this.state)
+      );
+    }
     this.render();
     return this;
   }
 
   mount() {
+    this.setEvent();
+    this.node.appendChild(this.childFragment);
     this.$target.appendChild(this.node);
     return this;
   }
 
   unMount() {
+    this.clearEvent();
+    this.node.removeChild(this.childFragment);
     this.$target.removeChild(this.node);
     return this;
   }
