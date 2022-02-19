751140f008f7ece77a8dfda9507fbeb7c76f8ddb
Alang Kim <rlaangh77@naver.com>
2022-02-19 15:34:33 +0900

    fix(frontend): form Component children prop 전달

--- src/public/js/components/Form.js
+++ src/public/js/components/Form.js
@@ -1,8 +1,8 @@
 import Component from "../Component.template.js";
 
 class Form extends Component {
-  constructor({ $target, initialState, onSubmit }) {
-    super({ $target, initialState });
+  constructor({ $target, initialState, onSubmit, children }) {
+    super({ $target, initialState, children });
     this.node = document.createElement("form");
 
     this.handleSubmit = (e) => {
