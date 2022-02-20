f98fc974d160fc0f0784c554bfe67e9015e5eb99
Alang Kim <rlaangh77@naver.com>
2022-02-20 18:30:49 +0900

    feat(frontend): select, Video Component

---
+++ src/public/js/components/Video.js
@@ -0,0 +1,35 @@
+import Component from "../Component.template.js";
+
+const defaultState = {
+  autoplay: false,
+  playsInline: false,
+  muted: false,
+  width: 100,
+  height: 100,
+  src: "",
+  srcObject: {},
+};
+
+class Video extends Component {
+  constructor({ $target, initialState }) {
+    super({ $target, initialState: { ...defaultState, ...initialState } });
+
+    this.node = document.createElement("video");
+  }
+
+  render() {
+    const { autoplay, playsInline, muted, width, height, src, srcObject } =
+      this.state;
+    this.node.autoplay = autoplay;
+    this.node.playsInline = playsInline;
+    this.node.muted = muted;
+    this.node.width = width;
+    this.node.height = height;
+    this.node.src = src;
+    this.node.srcObject = srcObject;
+
+    this.node.style.visibility = this.state.hidden ? "hidden" : "visible";
+  }
+}
+
+export default Video;
